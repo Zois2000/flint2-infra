62f12f4a2d14979b62d76a037dba92a9ed8ffbb5
Zois2000 <andreas.schweizer@mailbox.org>
2026-09-24 05:34:25 +0200

    configured iot wireless network

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,10 @@ chmod 600 /root/.ssh/flint2_ed25519
 cp /keys/known_hosts /root/.ssh/known_hosts
 chmod 644 /root/.ssh/known_hosts
 
+mkdir -p /root/.config/ansible
+chmod 700 /root/.config/ansible
+
+cp /secrets/vault-password /root/.config/ansible/vault-password
+chmod 600 /root/.config/ansible/vault-password
+
 exec "$@"
